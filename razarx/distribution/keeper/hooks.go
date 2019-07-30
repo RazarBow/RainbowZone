420c282eeb5f05f2004ed15aafb709bdb7739f1e
Razarbow <Razarbow@outlook.com>
2019-07-30 10:25:48 +0800

    fix distr initialization

--- razarx/distribution/keeper/hooks.go
+++ razarx/distribution/keeper/hooks.go
@@ -1,107 +1,97 @@
 package keeper
 
 import (
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/distribution/types"
-	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
+
+// Create a new validator distribution record
+func (k Keeper) onValidatorCreated(ctx sdk.Context, addr sdk.ValAddress) {
+
+	height := ctx.BlockHeight()
+	vdi := types.ValidatorDistInfo{
+		OperatorAddr:            addr,
+		FeePoolWithdrawalHeight: height,
+		Pool:           types.DecCoins{},
+		PoolCommission: types.DecCoins{},
+		DelAccum:       types.NewTotalAccum(height),
+	}
+	k.SetValidatorDistInfo(ctx, vdi)
+}
+
+// Withdrawal all validator rewards
+func (k Keeper) onValidatorCommissionChange(ctx sdk.Context, addr sdk.ValAddress) {
+	k.WithdrawValidatorRewardsAll(ctx, addr)
+}
+
+// Withdrawal all validator distribution rewards and cleanup the distribution record
+func (k Keeper) onValidatorRemoved(ctx sdk.Context, addr sdk.ValAddress) {
+	k.RemoveValidatorDistInfo(ctx, addr)
+}
+
+//_________________________________________________________________________________________
+
+// Create a new delegator distribution record
+func (k Keeper) onDelegationCreated(ctx sdk.Context, delAddr sdk.AccAddress,
+	valAddr sdk.ValAddress) {
+
+	ddi := types.DelegationDistInfo{
+		DelegatorAddr:    delAddr,
+		ValOperatorAddr:  valAddr,
+		WithdrawalHeight: ctx.BlockHeight(),
+	}
+	k.SetDelegationDistInfo(ctx, ddi)
+	ctx.Logger().With("module", "x/distribution").Error(fmt.Sprintf("ddi created: %v", ddi))
+}
+
+// Withdrawal all validator rewards
+func (k Keeper) onDelegationSharesModified(ctx sdk.Context, delAddr sdk.AccAddress,
+	valAddr sdk.ValAddress) {
+
+	k.WithdrawDelegationReward(ctx, delAddr, valAddr)
+}
+
+// Withdrawal all validator distribution rewards and cleanup the distribution record
+func (k Keeper) onDelegationRemoved(ctx sdk.Context, delAddr sdk.AccAddress,
+	valAddr sdk.ValAddress) {
+
+	k.RemoveDelegationDistInfo(ctx, delAddr, valAddr)
+}
+
+//_________________________________________________________________________________________
 
 // Wrapper struct
 type Hooks struct {
 	k Keeper
 }
 
-var _ stakingtypes.StakingHooks = Hooks{}
+var _ sdk.StakingHooks = Hooks{}
 
-// Create new distribution hooks
+// New Validator Hooks
 func (k Keeper) Hooks() Hooks { return Hooks{k} }
 
-// initialize validator distribution record
-func (h Hooks) AfterValidatorCreated(ctx sdk.Context, valAddr sdk.ValAddress) {
-	val := h.k.stakingKeeper.Validator(ctx, valAddr)
-	h.k.initializeValidator(ctx, val)
+// nolint
+func (h Hooks) OnValidatorCreated(ctx sdk.Context, addr sdk.ValAddress) {
+	h.k.onValidatorCreated(ctx, addr)
+}
+func (h Hooks) OnValidatorCommissionChange(ctx sdk.Context, addr sdk.ValAddress) {
+	h.k.onValidatorCommissionChange(ctx, addr)
+}
+func (h Hooks) OnValidatorRemoved(ctx sdk.Context, addr sdk.ValAddress) {
+	h.k.onValidatorRemoved(ctx, addr)
+}
+func (h Hooks) OnDelegationCreated(ctx sdk.Context, delAddr sdk.AccAddress, valAddr sdk.ValAddress) {
+	h.k.onDelegationCreated(ctx, delAddr, valAddr)
+}
+func (h Hooks) OnDelegationSharesModified(ctx sdk.Context, delAddr sdk.AccAddress, valAddr sdk.ValAddress) {
+	h.k.onDelegationSharesModified(ctx, delAddr, valAddr)
+}
+func (h Hooks) OnDelegationRemoved(ctx sdk.Context, delAddr sdk.AccAddress, valAddr sdk.ValAddress) {
+	h.k.onDelegationRemoved(ctx, delAddr, valAddr)
 }
 
-// cleanup for after validator is removed
-func (h Hooks) AfterValidatorRemoved(ctx sdk.Context, _ sdk.ConsAddress, valAddr sdk.ValAddress) {
-
-	// fetch outstanding
-	outstanding := h.k.GetValidatorOutstandingRewards(ctx, valAddr)
-
-	// force-withdraw commission
-	commission := h.k.GetValidatorAccumulatedCommission(ctx, valAddr)
-	if !commission.IsZero() {
-		// subtract from outstanding
-		outstanding = outstanding.Sub(commission)
-
-		// split into integral & remainder
-		coins, remainder := commission.TruncateDecimal()
-
-		// remainder to community pool
-		feePool := h.k.GetFeePool(ctx)
-		feePool.CommunityPool = feePool.CommunityPool.Add(remainder)
-		h.k.SetFeePool(ctx, feePool)
-
-		// add to validator account
-		if !coins.IsZero() {
-
-			accAddr := sdk.AccAddress(valAddr)
-			withdrawAddr := h.k.GetDelegatorWithdrawAddr(ctx, accAddr)
-			err := h.k.supplyKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, withdrawAddr, coins)
-			if err != nil {
-				panic(err)
-			}
-		}
-	}
-
-	// add outstanding to community pool
-	feePool := h.k.GetFeePool(ctx)
-	feePool.CommunityPool = feePool.CommunityPool.Add(outstanding)
-	h.k.SetFeePool(ctx, feePool)
-
-	// delete outstanding
-	h.k.DeleteValidatorOutstandingRewards(ctx, valAddr)
-
-	// remove commission record
-	h.k.DeleteValidatorAccumulatedCommission(ctx, valAddr)
-
-	// clear slashes
-	h.k.DeleteValidatorSlashEvents(ctx, valAddr)
-
-	// clear historical rewards
-	h.k.DeleteValidatorHistoricalRewards(ctx, valAddr)
-
-	// clear current rewards
-	h.k.DeleteValidatorCurrentRewards(ctx, valAddr)
-}
-
-// increment period
-func (h Hooks) BeforeDelegationCreated(ctx sdk.Context, delAddr sdk.AccAddress, valAddr sdk.ValAddress) {
-	val := h.k.stakingKeeper.Validator(ctx, valAddr)
-	h.k.incrementValidatorPeriod(ctx, val)
-}
-
-// withdraw delegation rewards (which also increments period)
-func (h Hooks) BeforeDelegationSharesModified(ctx sdk.Context, delAddr sdk.AccAddress, valAddr sdk.ValAddress) {
-	val := h.k.stakingKeeper.Validator(ctx, valAddr)
-	del := h.k.stakingKeeper.Delegation(ctx, delAddr, valAddr)
-	if _, err := h.k.withdrawDelegationRewards(ctx, val, del); err != nil {
-		panic(err)
-	}
-}
-
-// create new delegation period record
-func (h Hooks) AfterDelegationModified(ctx sdk.Context, delAddr sdk.AccAddress, valAddr sdk.ValAddress) {
-	h.k.initializeDelegation(ctx, valAddr, delAddr)
-}
-
-// record the slash event
-func (h Hooks) BeforeValidatorSlashed(ctx sdk.Context, valAddr sdk.ValAddress, fraction sdk.Dec) {
-	h.k.updateValidatorSlashFraction(ctx, valAddr, fraction)
-}
-
-// nolint - unused hooks
-func (h Hooks) BeforeValidatorModified(_ sdk.Context, _ sdk.ValAddress)                         {}
-func (h Hooks) AfterValidatorBonded(_ sdk.Context, _ sdk.ConsAddress, _ sdk.ValAddress)         {}
-func (h Hooks) AfterValidatorBeginUnbonding(_ sdk.Context, _ sdk.ConsAddress, _ sdk.ValAddress) {}
-func (h Hooks) BeforeDelegationRemoved(_ sdk.Context, _ sdk.AccAddress, _ sdk.ValAddress)       {}
+// nolint - unused hooks for interface
+func (h Hooks) OnValidatorBonded(ctx sdk.Context, addr sdk.ConsAddress)         {}
+func (h Hooks) OnValidatorBeginUnbonding(ctx sdk.Context, addr sdk.ConsAddress) {}
--- razarx/distribution/keeper/test_common.go
+++ razarx/distribution/keeper/test_common.go
@@ -17,13 +17,11 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/auth"
 	"github.com/cosmos/cosmos-sdk/x/bank"
 	"github.com/cosmos/cosmos-sdk/x/params"
-	"github.com/cosmos/cosmos-sdk/x/staking"
-	"github.com/cosmos/cosmos-sdk/x/supply"
+	"github.com/cosmos/cosmos-sdk/x/stake"
 
 	"github.com/cosmos/cosmos-sdk/x/distribution/types"
 )
 
-//nolint: deadcode unused
 var (
 	delPk1   = ed25519.GenPrivKey().PubKey()
 	delPk2   = ed25519.GenPrivKey().PubKey()
@@ -32,26 +30,17 @@ var (
 	delAddr2 = sdk.AccAddress(delPk2.Address())
 	delAddr3 = sdk.AccAddress(delPk3.Address())
 
-	valOpPk1    = ed25519.GenPrivKey().PubKey()
-	valOpPk2    = ed25519.GenPrivKey().PubKey()
-	valOpPk3    = ed25519.GenPrivKey().PubKey()
-	valOpAddr1  = sdk.ValAddress(valOpPk1.Address())
-	valOpAddr2  = sdk.ValAddress(valOpPk2.Address())
-	valOpAddr3  = sdk.ValAddress(valOpPk3.Address())
-	valAccAddr1 = sdk.AccAddress(valOpPk1.Address()) // generate acc addresses for these validator keys too
-	valAccAddr2 = sdk.AccAddress(valOpPk2.Address())
-	valAccAddr3 = sdk.AccAddress(valOpPk3.Address())
+	valPk1      = ed25519.GenPrivKey().PubKey()
+	valPk2      = ed25519.GenPrivKey().PubKey()
+	valPk3      = ed25519.GenPrivKey().PubKey()
+	valAddr1    = sdk.ValAddress(valPk1.Address())
+	valAddr2    = sdk.ValAddress(valPk2.Address())
+	valAddr3    = sdk.ValAddress(valPk3.Address())
+	valAccAddr1 = sdk.AccAddress(valPk1.Address()) // generate acc addresses for these validator keys too
+	valAccAddr2 = sdk.AccAddress(valPk2.Address())
+	valAccAddr3 = sdk.AccAddress(valPk3.Address())
 
-	valConsPk1   = ed25519.GenPrivKey().PubKey()
-	valConsPk2   = ed25519.GenPrivKey().PubKey()
-	valConsPk3   = ed25519.GenPrivKey().PubKey()
-	valConsAddr1 = sdk.ConsAddress(valConsPk1.Address())
-	valConsAddr2 = sdk.ConsAddress(valConsPk2.Address())
-	valConsAddr3 = sdk.ConsAddress(valConsPk3.Address())
-
-	// TODO move to common testing package for all modules
-	// test addresses
-	TestAddrs = []sdk.AccAddress{
+	addrs = []sdk.AccAddress{
 		delAddr1, delAddr2, delAddr3,
 		valAccAddr1, valAccAddr2, valAccAddr3,
 	}
@@ -65,9 +54,8 @@ var (
 func MakeTestCodec() *codec.Codec {
 	var cdc = codec.New()
 	bank.RegisterCodec(cdc)
-	staking.RegisterCodec(cdc)
+	stake.RegisterCodec(cdc)
 	auth.RegisterCodec(cdc)
-	supply.RegisterCodec(cdc)
 	sdk.RegisterCodec(cdc)
 	codec.RegisterCrypto(cdc)
 
@@ -75,88 +63,62 @@ func MakeTestCodec() *codec.Codec {
 	return cdc
 }
 
-// test input with default values
-func CreateTestInputDefault(t *testing.T, isCheckTx bool, initPower int64) (
-	sdk.Context, auth.AccountKeeper, Keeper, staking.Keeper, types.SupplyKeeper) {
-
-	communityTax := sdk.NewDecWithPrec(2, 2)
-
-	ctx, ak, _, dk, sk, _, supplyKeeper := CreateTestInputAdvanced(t, isCheckTx, initPower, communityTax)
-	return ctx, ak, dk, sk, supplyKeeper
-}
-
 // hogpodge of all sorts of input required for testing
-func CreateTestInputAdvanced(t *testing.T, isCheckTx bool, initPower int64,
-	communityTax sdk.Dec) (sdk.Context, auth.AccountKeeper, bank.Keeper,
-	Keeper, staking.Keeper, params.Keeper, types.SupplyKeeper) {
+func CreateTestInput(t *testing.T, isCheckTx bool, initCoins int64) (
+	sdk.Context, auth.AccountMapper, Keeper, stake.Keeper) {
 
-	initTokens := sdk.TokensFromConsensusPower(initPower)
-
-	keyDistr := sdk.NewKVStoreKey(types.StoreKey)
-	keyStaking := sdk.NewKVStoreKey(staking.StoreKey)
-	tkeyStaking := sdk.NewTransientStoreKey(staking.TStoreKey)
-	keyAcc := sdk.NewKVStoreKey(auth.StoreKey)
-	keySupply := sdk.NewKVStoreKey(supply.StoreKey)
-	keyParams := sdk.NewKVStoreKey(params.StoreKey)
-	tkeyParams := sdk.NewTransientStoreKey(params.TStoreKey)
+	keyDistr := sdk.NewKVStoreKey("distr")
+	tkeyDistr := sdk.NewTransientStoreKey("transient_distr")
+	keyStake := sdk.NewKVStoreKey("stake")
+	tkeyStake := sdk.NewTransientStoreKey("transient_stake")
+	keyAcc := sdk.NewKVStoreKey("acc")
+	keyFeeCollection := sdk.NewKVStoreKey("fee")
+	keyParams := sdk.NewKVStoreKey("params")
 
 	db := dbm.NewMemDB()
 	ms := store.NewCommitMultiStore(db)
 
+	ms.MountStoreWithDB(tkeyDistr, sdk.StoreTypeTransient, nil)
 	ms.MountStoreWithDB(keyDistr, sdk.StoreTypeIAVL, db)
-	ms.MountStoreWithDB(tkeyStaking, sdk.StoreTypeTransient, nil)
-	ms.MountStoreWithDB(keyStaking, sdk.StoreTypeIAVL, db)
-	ms.MountStoreWithDB(keySupply, sdk.StoreTypeIAVL, db)
+	ms.MountStoreWithDB(tkeyStake, sdk.StoreTypeTransient, nil)
+	ms.MountStoreWithDB(keyStake, sdk.StoreTypeIAVL, db)
 	ms.MountStoreWithDB(keyAcc, sdk.StoreTypeIAVL, db)
+	ms.MountStoreWithDB(keyFeeCollection, sdk.StoreTypeIAVL, db)
 	ms.MountStoreWithDB(keyParams, sdk.StoreTypeIAVL, db)
-	ms.MountStoreWithDB(tkeyParams, sdk.StoreTypeTransient, db)
 
 	err := ms.LoadLatestVersion()
 	require.Nil(t, err)
 
-	cdc := MakeTestCodec()
-	pk := params.NewKeeper(cdc, keyParams, tkeyParams, params.DefaultCodespace)
-
 	ctx := sdk.NewContext(ms, abci.Header{ChainID: "foochainid"}, isCheckTx, log.NewNopLogger())
-	accountKeeper := auth.NewAccountKeeper(cdc, keyAcc, pk.Subspace(auth.DefaultParamspace), auth.ProtoBaseAccount)
-	bankKeeper := bank.NewBaseKeeper(accountKeeper, pk.Subspace(bank.DefaultParamspace), bank.DefaultCodespace)
-	supplyKeeper := supply.NewKeeper(cdc, keySupply, accountKeeper, bankKeeper, supply.DefaultCodespace,
-		[]string{auth.FeeCollectorName, types.ModuleName}, []string{}, []string{staking.NotBondedPoolName, staking.BondedPoolName})
-
-	sk := staking.NewKeeper(cdc, keyStaking, tkeyStaking, supplyKeeper, pk.Subspace(staking.DefaultParamspace), staking.DefaultCodespace)
-	sk.SetParams(ctx, staking.DefaultParams())
-
-	keeper := NewKeeper(cdc, keyDistr, pk.Subspace(DefaultParamspace), sk, supplyKeeper, types.DefaultCodespace, auth.FeeCollectorName)
-
-	initCoins := sdk.NewCoins(sdk.NewCoin(sk.BondDenom(ctx), initTokens))
-	totalSupply := sdk.NewCoins(sdk.NewCoin(sk.BondDenom(ctx), initTokens.MulRaw(int64(len(TestAddrs)))))
-	supplyKeeper.SetSupply(ctx, supply.NewSupply(totalSupply))
+	cdc := MakeTestCodec()
+	accountMapper := auth.NewAccountMapper(cdc, keyAcc, auth.ProtoBaseAccount)
+	ck := bank.NewBaseKeeper(accountMapper)
+	sk := stake.NewKeeper(cdc, keyStake, tkeyStake, ck, stake.DefaultCodespace)
+	sk.SetPool(ctx, stake.InitialPool())
+	sk.SetParams(ctx, stake.DefaultParams())
+	sk.InitIntraTxCounter(ctx)
 
 	// fill all the addresses with some coins, set the loose pool tokens simultaneously
-	for _, addr := range TestAddrs {
-		_, err := bankKeeper.AddCoins(ctx, addr, initCoins)
+	for _, addr := range addrs {
+		pool := sk.GetPool(ctx)
+		_, _, err := ck.AddCoins(ctx, addr, sdk.Coins{
+			{sk.GetParams(ctx).BondDenom, sdk.NewInt(initCoins)},
+		})
 		require.Nil(t, err)
+		pool.LooseTokens = pool.LooseTokens.Add(sdk.NewDec(initCoins))
+		sk.SetPool(ctx, pool)
 	}
 
-	// create module accounts
-	feeCollectorAcc := supply.NewEmptyModuleAccount(auth.FeeCollectorName, supply.Basic)
-	notBondedPool := supply.NewEmptyModuleAccount(staking.NotBondedPoolName, supply.Burner)
-	bondPool := supply.NewEmptyModuleAccount(staking.BondedPoolName, supply.Burner)
-	distrAcc := supply.NewEmptyModuleAccount(types.ModuleName, supply.Basic)
-
-	keeper.supplyKeeper.SetModuleAccount(ctx, feeCollectorAcc)
-	keeper.supplyKeeper.SetModuleAccount(ctx, notBondedPool)
-	keeper.supplyKeeper.SetModuleAccount(ctx, bondPool)
-	keeper.supplyKeeper.SetModuleAccount(ctx, distrAcc)
+	fck := auth.NewFeeCollectionKeeper(cdc, keyFeeCollection)
+	pk := params.NewKeeper(cdc, keyParams)
+	keeper := NewKeeper(cdc, keyDistr, tkeyDistr, pk.Setter(), ck, sk, fck, types.DefaultCodespace)
 
 	// set the distribution hooks on staking
-	sk.SetHooks(keeper.Hooks())
+	sk = sk.WithHooks(keeper.Hooks())
 
 	// set genesis items required for distribution
 	keeper.SetFeePool(ctx, types.InitialFeePool())
-	keeper.SetCommunityTax(ctx, communityTax)
-	keeper.SetBaseProposerReward(ctx, sdk.NewDecWithPrec(1, 2))
-	keeper.SetBonusProposerReward(ctx, sdk.NewDecWithPrec(4, 2))
+	keeper.SetCommunityTax(ctx, sdk.NewDecWithPrec(2, 2))
 
-	return ctx, accountKeeper, bankKeeper, keeper, sk, pk, supplyKeeper
+	return ctx, accountMapper, keeper, sk
 }
